34a1a9c7440ffb273ff38bcfdba115564db08467
andreu <andreu@macbook-pro.local>
2014-01-04 18:13:22 +0100

    Function arguments to Super okClick.

--- src/app_paint/classes/BackgroundColorScreen.js
+++ src/app_paint/classes/BackgroundColorScreen.js
@@ -3,7 +3,7 @@ __CLASS__('BackgroundColorScreen', ColorModalScreen,
 {
     okClick: function(sender, e)
     {
-        this.Super('okClick')();
+        this.Super('okClick')(sender, e);
         
         this.SendOrderedBroadcast('SET_BACKGROUND_COLOR', this.color);
     }
--- src/app_paint/classes/LineColorScreen.js
+++ src/app_paint/classes/LineColorScreen.js
@@ -3,7 +3,7 @@ __CLASS__('LineColorScreen', ColorModalScreen,
 {
     okClick: function(sender, e)
     {
-        this.Super('okClick')();
+        this.Super('okClick')(sender, e);
         
         this.SendOrderedBroadcast('SET_LINE_COLOR', this.color);
     }
